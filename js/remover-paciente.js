2ca0fa70fb4d24b50297c9e5d4d1f209ba08a678
Igor Anthony <91284571+IgorAnthonyDeJesus@users.noreply.github.com>
2023-02-09 00:02:20 -0300

    Add files via upload

---
+++ js/remover-paciente.js
@@ -0,0 +1,12 @@
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    var alvoDoEvento = event.target;
+    var paiDoAlvo = alvoDoEvento.parentNode;
+
+    paiDoAlvo.classList.add("fadeOut");
+    setTimeout(function(){
+      paiDoAlvo.remove();  
+    }, 500);
+    
+})
